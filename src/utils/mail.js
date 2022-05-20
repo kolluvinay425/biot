f500a59a9606564d0d491e26aa21eecb93f1e8e7
vinay kollu <kolluvinay425@gmail.com>
2022-05-20 20:47:09 +0200

    from email changed

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -12,20 +12,16 @@ export const sendMail = async (recepient, id) => {
   const newFileName = `${id}.ics`;
 
   const filePath = path.join(publicFolderPath, newFileName);
-  console.log("filePath", filePath);
   fs.readFile(filePath, (err, data) => {
     if (err) {
       console.log("err", err);
-      // console.log("errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
     }
     if (data) {
-      console.log("data", data);
-
       const msg = {
         // to: recepient, //problem solved
         to: recepient,
 
-        from: "lonelyvinay76@gmail.com",
+        from: "serena_chatbot@biot.it",
         subject: "Attachment",
         html: "<strong>your consultation with biot successful!</strong>",
         attachments: [
